efd1191445a7b0c8e92cf59333542f581d4b54d2
levent <levent8421@outlook.com>
2019-09-10 18:51:02 +0800

    修正配置BUG

--- iot-templates/iot-templates-commons/src/main/java/com/monolithiot/iot/templates/context/TemplatesConstants.java
+++ iot-templates/iot-templates-commons/src/main/java/com/monolithiot/iot/templates/context/TemplatesConstants.java
@@ -12,7 +12,7 @@ public class TemplatesConstants {
      * 上下文相关常量
      */
     public static class Context {
-        public static final String COMPONENT_BASE_PACKAGE = "com.monolithiot.iot.templates";
+        public static final String COMPONENT_BASE_PACKAGE = "com.monolithiot.iot";
         public static final String MAPPER_PACKAGE_NAME = "com.monolithiot.iot.templates.repository.sql";
         public static final String SAFE_CONTROLLER_PACKAGE = "com.monolithiot.iot.templates.controller.safe";
         public static final String API_CONTROLLER_PACKAGE = "com.monolithiot.iot.templates.controller.api";
